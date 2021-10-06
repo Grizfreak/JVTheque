7ab347218cae7c01a71e21ffa33da97a0a8ab9d1
Maxime Guinoiseau <maxime.guinoiseau.etu@univ-lemans.fr>
2021-10-06 13:32:25 +0200

    Grid display + get image

--- app/src/main/java/com/jv/theque/GameAdapter.java
+++ app/src/main/java/com/jv/theque/GameAdapter.java
@@ -2,9 +2,6 @@ package com.jv.theque;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.drawable.Icon;
-import android.media.Image;
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -12,17 +9,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
-import java.io.BufferedInputStream;
-import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.List;
 
 public class GameAdapter extends RecyclerView.Adapter<GameAdapter.GameViewHolder> {
@@ -64,8 +52,12 @@ public class GameAdapter extends RecyclerView.Adapter<GameAdapter.GameViewHolder
         }
 
         void display(Game game) {
-            new DownloadImageTask((ImageView) gamePicture)
+            if(game.backgroundImageLink != null){
+
+
+            new DownloadImageTask(gamePicture)
                     .execute(game.backgroundImageLink);
+            }
 
             gameName.setText(game.name);
             if (game.platforms != null) {
@@ -76,29 +68,31 @@ public class GameAdapter extends RecyclerView.Adapter<GameAdapter.GameViewHolder
         private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
             ImageView bmImage;
 
-            public DownloadImageTask(ImageView bmImage) {
+            public DownloadImageTask(ImageView bmImage){
                 this.bmImage = bmImage;
             }
 
-            protected Bitmap doInBackground(String... urls) {
+         public  Bitmap doInBackground(String... urls) {
                 String urldisplay = urls[0];
                 Bitmap mIcon11 = null;
                 try {
                     InputStream in = new java.net.URL(urldisplay).openStream();
                     mIcon11 = BitmapFactory.decodeStream(in);
+                    bmImage.setImageBitmap(mIcon11);
+
                 } catch (Exception e) {
-                    Log.e("Error", e.getMessage());
-                    e.printStackTrace();
+                    Log.e("Error", " " +e);
+                    //e.printStackTrace();
                 }
-                return mIcon11;
+
+             return mIcon11;
             }
 
             protected void onPostExecute(Bitmap result) {
-                if(result == null){
-                    return;
-                }
                 bmImage.setImageBitmap(result);
+                Log.println(Log.INFO,"Image", "one image posted");
             }
+
         }
 
     }
