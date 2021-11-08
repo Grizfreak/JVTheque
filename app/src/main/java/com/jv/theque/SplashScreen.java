6680fd8b9568a0598536b953a1b081bd546b554a
EIdras <simon72610@gmail.com>
2021-11-08 21:16:12 +0100

    Splash screen functional, using a gif as logo

---
+++ app/src/main/java/com/jv/theque/SplashScreen.java
@@ -0,0 +1,22 @@
+package com.jv.theque;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.background_splash);
+
+        new Handler().postDelayed(() -> {
+            SplashScreen.this.startActivity(new Intent(SplashScreen.this, MainActivity.class));
+            SplashScreen.this.finish();
+        }, 4550);
+    }
+}
