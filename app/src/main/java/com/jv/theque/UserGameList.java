7f7b60608263290e78d8af361b3d36b6e189b3e4
Maxime Guinoiseau <maxime.guinoiseau.etu@univ-lemans.fr>
2021-11-12 09:16:59 +0100

    optimize save method on UserGameList implements get list from file on boot

--- app/src/main/java/com/jv/theque/UserGameList.java
+++ app/src/main/java/com/jv/theque/UserGameList.java
@@ -8,10 +8,15 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Serializable;
 import java.lang.reflect.Type;
@@ -19,6 +24,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserGameList implements Serializable {
+
+    private static final String storageFileName = "userGameList.bin";
 
     private final Type GAME_TYPE = new TypeToken<List<Game>>() {
     }.getType();
@@ -58,9 +65,16 @@ public class UserGameList implements Serializable {
     private ArrayList<Game> loadListFromFile() {
 
         try {
-            Gson gson = new Gson();
-            JsonReader reader = new JsonReader(new FileReader(new File(App.getAppContext().getFilesDir(), "userGameList.json")));
-            return new Gson().fromJson(String.valueOf(reader), GAME_TYPE);
+            File myObj = new File(App.getAppContext().getFilesDir(), storageFileName);
+            if (myObj.createNewFile()) {
+                System.out.println("File created: " + myObj.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+
+            FileInputStream inputStream = new FileInputStream(myObj.getAbsoluteFile());
+            ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+            return (ArrayList<Game>) objectInputStream.readObject();
 
         } catch (Exception e) {
             return new ArrayList<Game>();
@@ -70,18 +84,17 @@ public class UserGameList implements Serializable {
     public void saveToFile() {
 
         try {
-            String path = "userGameList.json";
 
-            File myObj = new File(App.getAppContext().getFilesDir(), path);
+            File myObj = new File(App.getAppContext().getFilesDir(), storageFileName);
             if (myObj.createNewFile()) {
                 System.out.println("File created: " + myObj.getName());
             } else {
                 System.out.println("File already exists.");
             }
 
-            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(new FileOutputStream(myObj.getAbsoluteFile()));
-            outputStreamWriter.write(String.valueOf(list));
-            outputStreamWriter.close();
+            FileOutputStream outputStream = new FileOutputStream(myObj.getAbsoluteFile());
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+            objectOutputStream.writeObject(list);
 
         } catch (IOException e) {
             e.printStackTrace();
